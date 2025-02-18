b1706ad86a4c7f3241ca45ee82b2a3e443dd1574
Phantom <brijeshbabu2001@gmail.com>
2025-02-19 00:57:43 +0530

    CI/CD

--- tests/test_ai_chatbot.py
+++ tests/test_ai_chatbot.py
@@ -1,6 +1,10 @@
 import pytest
 from packages.ai_chatbot import chatbot
 
-def test_chatbot():
-    assert chatbot('Give only the answer for 1+1') == '2'
 
+def test_chatbot():
+    try:
+        response = chatbot('Give only the answer for 1+1')
+    except:
+        response = 'Error from ai_chatbot'
+    assert response == '2'
