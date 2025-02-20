562ba8f5acda5730f770e268c6968fe19b611b1f
Phantom <brijeshbabu2001@gmail.com>
2025-02-21 02:14:11 +0530

    pytest update

--- tests/test_ai_chatbot.py
+++ tests/test_ai_chatbot.py
@@ -1,10 +1,17 @@
 import pytest
 from packages.ai_chatbot import chatbot
-
+chat = [
+    {"role": "system",
+     "content": "You are a helpful ai assistant"}
+]
 
 def test_chatbot():
+    global chat
+    convo = chat
+    convo.append({"role":"user",
+                 "content":"Give only the answer for 1+1"})
     try:
-        response = chatbot('Give only the answer for 1+1')
+        response = chatbot(convo)
     except:
         response = 'Error from ai_chatbot'
     assert response == '2'
