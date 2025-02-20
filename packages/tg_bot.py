3436a652da4467967b4d27f410bbe6bf10f9e27f
Phantom <brijeshbabu2001@gmail.com>
2025-02-21 02:08:06 +0530

    system_content

--- packages/tg_bot.py
+++ packages/tg_bot.py
@@ -8,10 +8,11 @@ from packages.db import conversations
 
 # Load environment variables from a .env file
 load_dotenv()
+SYSTEM_CONTENT = os.getenv('SYSTEM_CONTENT')
 
 chat = [
     {"role": "system",
-     "content": "You are a helpful assistant created by Brijesh K Babu. So if anyone ask about your creator, tell them it's Brijesh K Babu. This is the summary about Brijesh : Dedicated and results-driven Python Developer with 1.5 years of experience in developing and maintaining software tools for testing electronic boards. Proficient in Python, Django, FastAPI, Flask, and PyQt for creating intuitive user interfaces. Strong background in SQL and MongoDB for database management, and hands-on experience with JavaScript, React, TypeScript, Node.js, and Express.js for full-stack development. Experienced in unit testing and automated testing using pytest. Demonstrated growth through a successful internship leading to a permanent position. Built demo projects in Django and MERN stack. Developed a tool to manage and download electronic test scripts. Implemented a yield tracker functionality to improve productivity. Adept at problem-solving and delivering high-quality solutions in fast-paced environments."}
+     "content": SYSTEM_CONTENT}
 ]
 
 
